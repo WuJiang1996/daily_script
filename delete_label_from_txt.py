f4c3043a64a9b20de4a8b4275b34ed3056b91828
wujiang <1225141534@qq.com>
2022-12-01 14:19:38 +0800

    update some py files

--- delete_label_from_txt.py
+++ delete_label_from_txt.py
@@ -1,24 +1,32 @@
 import os
-import shutil
 dir = './labels/'
 
 for filename in os.listdir(dir):
     # print(filename)
+    label_list = []
     txt_path = dir + filename
-    with open(txt_path) as f:
-        context = f.readlines()
-        length = len(context)
-        for i in range(length):
-            # print(int(context[i][0]))
-            if int(context[i][0]) == 5:
-                del context[i]
+    if filename.endswith('txt'):
+        print(txt_path)
+        with open(txt_path) as f:
+            context = f.readlines()
+            length = len(context)
+            if length > 0:
+                for i in range(length):
+                    print(int(context[i][0]))
+                    if int(context[i][0]) != 5:
+                        label_list.append(i)
+                        # del context[i]
 
-    with open(txt_path) as f:
-        outPutDirgzName = 'new_label'
-        outPutDirgzPath = outPutDirgzName
-        if not os.path.exists(outPutDirgzPath):
-            os.makedirs(outPutDirgzPath)
-        file_new_path = outPutDirgzName + filename 
-        file_new = open(file_new_path,'w')
-        file_new.writelines(context[i]) # 将删除行后的数据写入文件
-        file_new.close()
+        print("label_list:", label_list)
+        with open(txt_path) as f:
+            outPutDirgzName = 'new_label'
+            outPutDirgzPath = outPutDirgzName
+            if not os.path.exists(outPutDirgzPath):
+                os.makedirs(outPutDirgzPath)
+            file_new_path = outPutDirgzName + '/'+filename 
+            print('file_new_path:', file_new_path)
+            file_new = open(file_new_path,'w')
+            for j in label_list:
+                file_new.writelines(context[j]) # 将删除行后的数据写入文件
+            file_new.close()
+            print('done!')
